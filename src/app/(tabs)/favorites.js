193b697a3d5190f410e418742c60c23ad1f735a0
DevDaniel21 <danielglaucia2.007@gmail.com>
2025-09-22 17:46:44 -0300

    feat: estilizado favoritos e criado card favorito

--- src/app/(tabs)/favorites.js
+++ src/app/(tabs)/favorites.js
@@ -1,9 +1,58 @@
 import { View, Text, StyleSheet } from 'react-native';
+import CardFavorite from '../components/CardFavorite';
+import Foundation from '@expo/vector-icons/Foundation';
 
 export default function Favorites() {
+    const favorites = [
+        {
+            id: '1',
+            name: 'Produto favorito com um nome bem extenso para ficar dificil de ler',
+            price: '99,90',
+            image: 'https://m.media-amazon.com/images/I/611drnhmwML._AC_SY450_.jpg',
+        },
+        {
+            id: '2',
+            name: 'Produto 2',
+            price: '49,90',
+            image: 'https://m.media-amazon.com/images/I/611drnhmwML._AC_SY450_.jpg',
+        },
+        {
+            id: '3',
+            name: 'Produto 3',
+            price: '29,90',
+            image: 'https://m.media-amazon.com/images/I/611drnhmwML._AC_SY450_.jpg',
+        },
+        {
+            id: '4',
+            name: 'Produto 4',
+            price: '19,90',
+            image: 'https://m.media-amazon.com/images/I/611drnhmwML._AC_SY450_.jpg',
+        },
+        {
+            id: '5',
+            name: 'Produto 5',
+            price: '9,90',
+            image: 'https://m.media-amazon.com/images/I/611drnhmwML._AC_SY450_.jpg',
+        },
+    ];
     return (
         <View style={styles.container}>
-            <Text>Favoritos</Text>
+            <View style={styles.title_container}>
+                <Foundation name="heart" size={40} color="#fff" />
+                <Text style={styles.title}>Favoritos</Text>
+            </View>
+
+            <View style={styles.favorites}>
+                {favorites.map((favorite) => (
+                    <CardFavorite
+                        key={favorite.id}
+                        id={favorite.id}
+                        name={favorite.name} 
+                        price={favorite.price}
+                        image={favorite.image}
+                        />
+                ))}
+            </View>
         </View>
     );
 }
@@ -16,5 +65,23 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start',
         alignItems: 'flex-start',
         backgroundColor: '#303030',
+    },
+    title_container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 10,
+    },
+    title: {
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: '700',
+    },
+    favorites: {
+        marginTop: 20,
+        width: '100%',
+        gap: 20,
+        flex: 1,
+        alignItems: 'center',
+        overflow: 'scroll'
     },
 });
